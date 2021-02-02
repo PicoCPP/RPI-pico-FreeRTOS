67f48cb9cb127dc655e54086710d37a25f7f65a1
PicoCPP <PicoCPP>
2021-02-02 19:51:12 +0000

    Static Allocation has to be disabled otherwise a hard fault occurs.

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -29,7 +29,7 @@
 #define configMESSAGE_BUFFER_LENGTH_TYPE        size_t
 
 /* Memory allocation related definitions. */
-#define configSUPPORT_STATIC_ALLOCATION         1
+#define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
 #define configTOTAL_HEAP_SIZE                   50000
 #define configAPPLICATION_ALLOCATED_HEAP        0
